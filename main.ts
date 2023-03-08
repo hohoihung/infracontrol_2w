19b3e8316e80ca5f81b77510ee84d2484f3db02e
hohoihung <w10546.ho@gmail.com>
2023-03-08 18:41:55 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,9 +19,43 @@ function moveForward () {
     pins.analogWritePin(AnalogPin.P13, _moveSpeed)
     _inMotion = 1
 }
+function moveM2 (rotateDir: string) {
+    if (rotateDir == "CW") {
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    } else if (rotateDir == "CCW") {
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
+    if (_moveSpeed <= speedMin) {
+        _moveSpeed = speedMin
+    } else if (_moveSpeed >= speedMax) {
+        _moveSpeed = speedMax
+    }
+    pins.analogWritePin(AnalogPin.P12, _moveSpeed)
+    _inMotion = 1
+}
 input.onButtonPressed(Button.A, function () {
-	
+    serial.writeValue("Move Speed", _moveSpeed)
+    _moveSpeed += 1
+    moveM1("CW")
 })
+function moveM1 (rotateDir: string) {
+    if (rotateDir == "CW") {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P9, 0)
+    } else if (rotateDir == "CCW") {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P9, 0)
+    }
+    if (_moveSpeed <= speedMin) {
+        _moveSpeed = speedMin
+    } else if (_moveSpeed >= speedMax) {
+        _moveSpeed = speedMax
+    }
+    pins.analogWritePin(AnalogPin.P4, _moveSpeed)
+    _inMotion = 1
+}
 pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
     _fallFlag = 1
 })
@@ -37,10 +71,7 @@ function moveStop () {
     pins.analogWritePin(AnalogPin.P13, 0)
     _inMotion = 0
 }
-pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
-    _collisionFlag = 1
-})
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.AB, function () {
     serial.writeValue("pin 0", pins.digitalReadPin(DigitalPin.P0))
     serial.writeValue("pin 1", pins.digitalReadPin(DigitalPin.P1))
     serial.writeValue("pin 2", pins.digitalReadPin(DigitalPin.P2))
@@ -56,6 +87,14 @@ input.onButtonPressed(Button.B, function () {
     serial.writeValue("Collision Fag", _collisionFlag)
     serial.writeValue("Fall Fag", _fallFlag)
 })
+pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
+    _collisionFlag = 1
+})
+input.onButtonPressed(Button.B, function () {
+    serial.writeValue("Move Speed", _moveSpeed)
+    _moveSpeed += -1
+    moveM1("CW")
+})
 pins.onPulsed(DigitalPin.P12, PulseValue.Low, function () {
     _collisionFlag = 1
 })
@@ -64,10 +103,14 @@ function moveLeft () {
 }
 let _inMotion = 0
 let _moveSpeed = 0
+let speedMax = 0
+let speedMin = 0
 let _fallFlag = 0
 let _collisionFlag = 0
 _collisionFlag = 0
 _fallFlag = 0
+speedMin = 20
+speedMax = 150
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
